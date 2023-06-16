d4e35dfa1dcdbf1f261890cea6dded714d57af08
fzarri <49104658+fzarri@users.noreply.github.com>
2023-06-16 13:05:42 +0200

    Update set-local-vars.sh

--- set-local-vars.sh
+++ set-local-vars.sh
@@ -3,7 +3,7 @@
 
 
 
-export OCI_DATASAFE_COMPARTMENT_OCID="ocid1.compartment.oc1..aaaaaaaa5jgku756s6f5studzgfnpqwffyv4tiaopp37cmcwqymuxga2edaa"
-export OCI_LOGGING_LOG_OCID="ocid1.log.oc1.eu-frankfurt-1.amaaaaaaupfargiadqyzelwuvgymmudhxxbm2uterosnjjalu55zojcns6wa"
+export OCI_DATASAFE_COMPARTMENT_OCID="ocid1.compartment.oc1..xxxxx"
+export OCI_LOGGING_LOG_OCID="ocid1.log.oc1.eu-frankfurt-1.xxxx"
 export OCI_OS_BUCKET_NAME="datasafeocilogging"
 export FN_APP_NAME="oci-datasafe-audit-to-logging"
