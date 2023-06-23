be2d016e44919580a249d0d1daa6a9dd5f65780b
Fabrizio Zarri <fzarri@gmail.com>
2023-06-23 17:13:36 +0200

    Last Update

---
+++ terraform/schedule.tf
@@ -0,0 +1,17 @@
+## Copyright (c) 2023, Oracle and/or its affiliates.
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+
+resource "oci_ons_notification_topic" "schedule_ds_ocilogging_notification_topic" {
+    #Required
+    compartment_id = var.compartment_ocid
+    name = "${var.NotificationTopicNamePrefix}-${var.deployment_name}-${random_id.tag.hex}"
+}
+
+resource "oci_ons_subscription" "schedule_ds_ocilogging_notification_subscription" {
+    #Required
+    compartment_id = var.compartment_id
+    endpoint = var.oci_functions_function.postauditlogs.id
+    protocol = "ORACLE_FUNCTIONS"
+    topic_id = oci_ons_notification_topic.schedule_ds_ocilogging_notification_topic.id
+}
