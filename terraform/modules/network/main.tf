b7755e277eefa7e0856bbe29cbe07031637ee2b5
Fabrizio Zarri <fzarri@gmail.com>
2023-06-27 10:56:22 +0200

    Last Update

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,5 +1,40 @@
-## Copyright (c) 2023, Oracle and/or its affiliates.
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+################################################################################
+#Copyright (c) 2023 Oracle and/or its affiliates.
+# 
+#The Universal Permissive License (UPL), Version 1.0
+# 
+#Subject to the condition set forth below, permission is hereby granted to any
+#person obtaining a copy of this software, associated documentation and/or data
+#(collectively the "Software"), free of charge and under any and all copyright
+#rights in the Software, and any and all patent rights owned or freely
+#licensable by each licensor hereunder covering either (i) the unmodified
+#Software as contributed to or provided by such licensor, or (ii) the Larger
+#Works (as defined below), to deal in both
+# 
+#(a) the Software, and
+#(b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+#one is included with the Software (each a "Larger Work" to which the Software
+#is contributed by such licensors),
+ 
+#without restriction, including without limitation the rights to copy, create
+#derivative works of, display, perform, and distribute the Software and make,
+#use, sell, offer for sale, import, export, have made, and have sold the
+#Software and the Larger Work(s), and to sublicense the foregoing rights on
+#either these or other terms.
+ 
+#This license is subject to the following condition:
+#The above copyright notice and either this complete permission notice or at
+#a minimum a reference to the UPL must be included in all copies or
+#substantial portions of the Software.
+ 
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+################################################################################
 
 resource "oci_core_virtual_network" "vcn" {
   cidr_block     = var.VCN-CIDR
@@ -12,6 +47,26 @@ resource "oci_core_nat_gateway" "nat_gateway" {
   compartment_id = var.compartment_ocid
   display_name   = "${var.fnvcnnamenatgatewayprefix}-${random_id.tag.hex}"
   vcn_id         = oci_core_virtual_network.vcn.id
+}
+
+data "oci_core_services" "service_gateway_all_oci_services" {
+  filter {
+    name   = "name"
+    values = ["All .* Services In Oracle Services Network"]
+    regex  = true
+  }
+}
+
+resource "oci_core_service_gateway" "service_gw" {
+
+  compartment_id = var.compartment_ocid
+  vcn_id          = oci_core_virtual_network.vcn.id
+  services {
+        service_id = lookup(data.oci_core_services.service_gateway_all_oci_services.services[0], "id")
+  }
+  display_name = var.service_gateway_display_name
+  
+  count = var.create_service_gateway == true ? 1 : 0
 }
 
 resource "oci_core_route_table" "rt_fn_subnet" {
